fcd85e46b5872b542361889d72f0dbe12e63bcdf
zzzzfz <chives0066@gmail.com>
2024-09-23 14:55:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import DOCS from './index.html'
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -43,6 +44,15 @@ async function handleRequest(request) {
       }
     );
   }
+// return index.html
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
